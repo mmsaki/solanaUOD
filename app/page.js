d5b9674ca4a66237a58c8f753baa4938f61631b7
Meek Msaki <meek10x@gmail.com>
2024-03-10 10:03:31 +0000

    adding suprise loader

--- app/page.js
+++ app/page.js
@@ -8,6 +8,20 @@ import { useState } from 'react';
 export default function Home() {
   // states
   const [error, setError] = useState('');
+  const [value, setValue] = useState('');
+  const [chatHistory, setChatHistory] = useState([]);
+
+  const supriseOptions = [
+    'How is solana performing in the last hour. Please give elaborate answer but start with Up or Down?',
+    'What is the best token in Solana?',
+    'What tokens are tredning in Solana right now?',
+  ];
+
+  const surpriseMe = () => {
+    const randomValue = supriseOptions[Math.floor(Math.random() * supriseOptions.length)];
+    setValue(randomValue);
+    return randomValue;
+  };
 
   // Initialize the Genereative Models
   if (process.env.NEXT_PUBLIC_GEMINI_API === undefined) console.error('API KEY not loaded');
@@ -64,25 +78,39 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
 
-      <div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
+      <div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-1 lg:text-left'>
         <section>
-          <button className={`mb-3 text-2xl font-semibold rounded-lg border border-transparent px-5 py-4 transition-colors hover:dark:bg-neutral-800/30`} onClick={() => askAI()}>
-            Ask AI <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>-&gt;</span>
-          </button>
           <div className='input-container'>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`} id='response-output'>
-              What do you want to know?
-            </p>
-            <input
-              className='m-2 text-gray-400 font-base rounded-lg border px-2 py-2 transition-colors hover:dark:bg-neutral-800/3'
-              onChange={() => {}}
-              value={''}
-              placeholder={'Write your prompt here...'}
-            />
+            <div className='flex flex gap-2 align-center p-2'>
+              <p className={`m-0 max-w-[50ch] text-sm opacity-50`} id='response-output'>
+                What do you want to know?
+              </p>
+              <button
+                className={`bg-neutral-200 rounded-md px-2 border border-transparent transition-colors dark:bg-neutral-900 hover:dark:bg-neutral-800/30 cursor-pointer`}
+                onClick={surpriseMe}
+                disabled={!chatHistory}>
+                suprise me
+              </button>
+            </div>
+            <div className='w-full flex'>
+              <input
+                className='grow text-gray-400 font-base rounded-lg border px-2 py-2 transition-colors hover:dark:bg-neutral-800/3'
+                onChange={(e) => setValue(e.target.value)}
+                value={value}
+                placeholder={'Write your prompt here...'}
+              />
+              <button
+                className={`grow-0 mb-3 px-2 py-2 text-2xl font-semibold rounded-lg border border-transparent transition-colors hover:dark:bg-neutral-800/30 cursor-pointer`}
+                onClick={() => askAI()}>
+                Ask AI <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>-&gt;</span>
+              </button>
+            </div>
           </div>
           {error && <p className='text-red-700'>{error}</p>}
-          <div key={{}}>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`} id='answer'></p>
+          <div className='mt-10 overflow-scroll'>
+            <div key={{}} className='flex border rounded-m p-4'>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`} id='answer'></p>
+            </div>
           </div>
         </section>
       </div>
