a29b792174a9e14e1d840a4fa858c98732e749a0
Meek Msaki <meek10x@gmail.com>
2024-03-10 08:27:06 +0000

    make prompt section

--- app/page.js
+++ app/page.js
@@ -61,16 +61,22 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
 
       <div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
-        <button
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          rel='noopener noreferrer'>
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+        <section>
+          <h2 className={`mb-3 text-2xl font-semibold rounded-lg border border-transparent px-5 py-4 transition-colors hover:dark:bg-neutral-800/30`}>
             Ask AI <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>-&gt;</span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`} id='response-output'>
-            Your in-depth information response will populate here.
-          </p>
-        </button>
+          <div className='input-container'>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`} id='response-output'>
+              What do you want to know?
+            </p>
+            <input
+              className='m-2 text-gray-400 font-base rounded-lg border px-2 py-2 transition-colors hover:dark:bg-neutral-800/3'
+              onChange={''}
+              value={''}
+              placeholder={'Write your prompt here...'}
+            />
+          </div>
+        </section>
       </div>
     </main>
   );
